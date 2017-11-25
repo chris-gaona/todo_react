4b08713b998c9941d0348377d7b5b1755fec5e7c
Chris Gaona <thebeloved88@sbcglobal.net>
2017-11-25 14:09:22 -0800

    Filters data based on property values

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {TodoForm, TodoList, Footer} from "./components/todo";
-import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo} from "./lib/todoHelpers";
+import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from "./lib/todoHelpers";
 import {pipe, partial} from "./lib/utils";
+import PropTypes from 'prop-types';
 
 class App extends Component {
 
@@ -14,6 +15,10 @@ class App extends Component {
       { id: 3, name: "Ship It!", isComplete: false }
     ],
     currentTodo: ""
+  };
+
+  static contextTypes = {
+    route: PropTypes.string
   };
 
   handleRemove = (id, e) => {
@@ -62,6 +67,7 @@ class App extends Component {
   render() {
 
     const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit;
+    const displayTodos = filterTodos(this.state.todos, this.context.route);
 
     return (
       <div className="App">
@@ -81,7 +87,7 @@ class App extends Component {
 
           <TodoList
             handleToggle={this.handleToggle}
-            todos={this.state.todos}
+            todos={displayTodos}
             handleRemove={this.handleRemove} />
 
           <Footer />
