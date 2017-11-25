615d5f0372f2897376f2152694e7b88c72ba4abe
Chris Gaona <thebeloved88@sbcglobal.net>
2017-11-25 14:11:56 -0800

    Keeps application state in sync with browser history

--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -31,6 +31,12 @@ export class Router extends Component {
     }
   }
 
+  componentDidMount() {
+    window.onpopstate = () => {
+      this.setState({route: getCurrentPath()});
+    }
+  }
+
   render() {
     return (
       <div>{this.props.children}</div>
