6b9fbde4bd24e2f9aea2126e068c2b02c164b510
Chris Gaona <thebeloved88@sbcglobal.net>
2017-11-24 20:27:27 -0800

    Uses React context to manage application state through routes

--- src/components/router/Link.js
+++ src/components/router/Link.js
@@ -2,16 +2,24 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 export class Link extends Component {
+
+  static contextTypes = {
+    route: PropTypes.string,
+    linkHandler: PropTypes.func,
+  };
+
   handleClick = (e) => {
     e.preventDefault();
 
-    //eslint-disable-next-line
-    history.pushState(null, "", this.props.to);
+    this.context.linkHandler(this.props.to);
   };
 
   render() {
+
+    const activeClass = this.context.route === this.props.to ? 'active' : '';
+
     return (
-      <a href="#" onClick={this.handleClick}>{this.props.children}</a>
+      <a href="#" className={activeClass} onClick={this.handleClick}>{this.props.children}</a>
     )
   }
 }
