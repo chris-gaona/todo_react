8ddd1b30abfaa765937fda77b86954b947d45d04
Chris Gaona <thebeloved88@sbcglobal.net>
2017-11-24 19:50:46 -0800

    Creates a pipe function to enable function composition

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,7 @@
 // first ...args takes multiple arguments and turns them into an array
 // seconds ...args spreads array back out as arguments into bind
 export const partial = (fn, ...args) => fn.bind(null, ...args);
+
+const _pipe = (fn, fn2) => (...args) => fn2(fn(...args));
+
+export const pipe = (...fns) => fns.reduce(_pipe);
